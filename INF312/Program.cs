8e1a5603fc0a7801567be5abb537013207566e54
Noubissie237 <wilfried.noubissie@facsciences-uy1.cm>
2024-05-10 02:23:39 +0200

    Version 1 : Avec beaucoup d'erreurs

--- INF312/Program.cs
+++ INF312/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ScottPlot;
 using System.Drawing;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace INF312
 {
@@ -20,10 +21,70 @@ namespace INF312
             return filename;
 
         }
+        public static List<string[]> GetData(string path, string filename)
+        {
+            var data = new List<string[]>();
+            try
+            {
+                using (StreamReader reader = new StreamReader(Path.Combine(path, filename)))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] fields = line.Split(',');
+                        data.Add(fields);
+                    }
+
+                }
+                data.RemoveAt(0);
+                foreach (var field in data)
+                {
+                    field[0] = field[0].Replace('-', ' ');
+                }
+
+                return data;
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Erreur : " + ex.Message);
+                return null;
+            }
+        }
+
+        public static void SavePicture(string filename, List<string[]> data, string title, string xLabel, string yLabel)
+        {
+            var dataX = new List<string>();
+            var dataY = new List<string>();
+
+            var plt = new Plot();
+
+            foreach (var elt in data)
+            {
+                dataX.Add(elt[0]);
+                dataY.Add(elt[1]);
+            }
+
+            plt.Add.Scatter(dataX, dataY);
+
+            plt.Title(title);
+            plt.XLabel(xLabel);
+            plt.YLabel(yLabel);
+
+            plt.Save(filename, 900, 800);
+
+        }
         public static void Main(string[] args)
         {
             var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "INF312_IMG");
+            var pathDataset = "N:\\UY1\\L3\\S02\\INF312(STAT)\\Nzekon\\Devoirs\\devoirNzekong";
             
+            var data1 = GetData(pathDataset, "AirPassengers.csv");
+            var data2 = GetData(pathDataset, "annual_rainfall_dallas.csv");
+            var data3 = GetData(pathDataset, "monthly-sunspots.csv");
+            //var data4 = GetData(pathDataset, "AirPassengers.csv");
+            //var data5 = GetData(pathDataset, "AirPassengers.csv");
+
+        
             if (!Directory.Exists(path))
             {
                 Directory.CreateDirectory(path);
@@ -31,19 +92,10 @@ namespace INF312
 
             string filename = GetFilename(path, 1);
 
-            var plt = new Plot();
+            SavePicture(filename, data1, "Air passengers", "Années", "Nombre de passager");
+            SavePicture(filename, data2, "annual_rainfall_dallas", "Années", "Nombre de pluie");
+            SavePicture(filename, data3, "Monthly-sunspots", "Années", "Sunspots");
 
-            var dataX = new double[] { 1, 2, 3, 4, 5 };
-            var dataY = new double[] { 1, 4, 9, 16, 25 };
-
-            plt.Add.Scatter(dataX, dataY);
-
-            plt.Title("Simple Plot");
-            plt.XLabel("X");
-            plt.YLabel("Y");
-
-            plt.Save(filename, 900, 800);
-    
             Console.WriteLine($"Image générée avec succès dans {path}");
         }
     }
